991c510639bdfa4319e735a4e94c3762e5ae082a
Chris Nogales <nogalesc@vt.edu>
2015-03-09 11:37:45 -0400

    expanded H and fixed indexing access

--- HW2/loocvreg_slow.m
+++ HW2/loocvreg_slow.m
@@ -6,32 +6,22 @@ function [ rmse ] = loocvreg_slow(xTr,yTr)
 %   xTr = 6x93
 %   yTr = 1x93
 %% Set up variables (using column vectors)
-n = size(xTr,2);               % sample size
-vertcat(ones(1,n), xTr);       % xTr = 7x93
-X = xTr';                      % X = 93x7
-Y = yTr';                      % Y = 93x1
+n = size(xTr,2);                           % sample size
+xTr = vertcat(ones(1,n), xTr);             % xTr = 7x93
+X = xTr';                                  % X = 93x7
+Y = yTr';                                  % Y = 93x1
 m_rmse = zeros(size(yTr'));    
 %% Naive LOOCV
 for k=1:size(xTr,2)                        % run 93 times
-    k
-    X(k,:) = [];                           % delete kth
-    Y(k,:) = [];           
-    H = X*pinv(X);     
-    Y_hat = H*Y;
+    X(k,:) = [];                           % delete a kth sample
+    Y(k,:) = [];      
+    cur_n = size(X,1);                     % update n
+    w_hat = pinv(X)*Y;     
+    Y_hat = X*w_hat;
     loocv_error = sum((Y - Y_hat).^2);
-    m_rmse(k) = sqrt((1/n)*loocv_error)
+    m_rmse(k) = sqrt((1/cur_n)*loocv_error);
     X = xTr';                               % add kth back
     Y = yTr';
 end
-% %% Nested fuctions
-% % Calculate squared difference
-% function sum_sqrd_error = sum_of_sqrd_diff(y,y_hat)
-%   sum_sqrd_error = (y - y_hat).^2; 
-% end
 rmse = min(m_rmse);
 end
-
-
-
-    %pinv
-    % y = H*y
