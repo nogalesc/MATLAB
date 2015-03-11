3876916d3596f0d07a760f76c18b18e8268cb58e
Chris Nogales <nogalesc@vt.edu>
2015-03-11 13:34:48 -0400

    Completed problem 2 part 8

--- HW2/loocvreg_fast.m
+++ HW2/loocvreg_fast.m
@@ -3,24 +3,32 @@ function [ rmse ] = loocvreg_fast(xTr,yTr)
 %   This function calculates the leave-on-out 
 %   cross-validation rmse, or
 %   'root mean squared error' (rmse) 
-%   xTr = 6x93
-%   yTr = 1x93
 %% Set up variables (using column vectors)
-n = size(xTr,2);                           % sample size
-xTr = vertcat(ones(1,n), xTr);             % xTr = 7x93
-X = xTr';                                  % X = 93x7
-Z = yTr';                                  % Z = 93x`
-Y = yTr';                                  % Y = 93x1
+% xTr 6 features x 93 samples
+% yTr 1x93 labels
+% X 93 samples x 6 features 
+% y 93x1 labels
+X = xTr';                                  
+y = yTr';   
+% Number of Samples n = 93
+n = size(y,1);
+% Add bias feature (ones) to each sample
+% X 93 samples x 7 features 
+X = [ones(n,1) X];
+%% Calculate rmse fast
+% Matrix that hold rmse values at each iteration
+m_loocv_error = zeros(n,1);    
+% Calculate the H matrix
+H = X*pinv(X'*X)*X';
+% Return column vector of the main diagonal elements of H.
+Hkk = diag(H);
+% Calculate y_hat
+y_hat = H*y;
+% The rmse error is calculated for each time you remove a sample.
+for k=1:n   
+    m_loocv_error(k) = ((y(k) - y_hat(k))/(1-Hkk(k)))^2;
+end
+% Add up all the squared errors to the loocv error variable
+loocv = sum(m_loocv_error);
+rmse = sqrt((1/n)*loocv);
 
-m_rmse = zeros(size(yTr'));    
-%% fast LOOCV
-for k=1:size(xTr,2)                        % run 93 times
-    H = X*pinv(X);                         % get H before removing kth
-%     Y(k,:) = [];                         % remove kth?
-%     n = size(Y,1);
-    loocv_error = sum(((Y(k) - H(k,k)*Y(k))/(1-H(k,k))).^2);
-    m_rmse(k) = sqrt((1/n)*loocv_error);
-    Y = yTr';
-end
-rmse = min(m_rmse);
-end
--- HW2/loocvreg_slow.m
+++ HW2/loocvreg_slow.m
@@ -3,25 +3,38 @@ function [ rmse ] = loocvreg_slow(xTr,yTr)
 %   This function calculates the leave-on-out 
 %   cross-validation rmse, or
 %   'root mean squared error' (rmse) 
-%   xTr = 6x93
-%   yTr = 1x93
 %% Set up variables (using column vectors)
-n = size(xTr,2);                           % sample size
-xTr = vertcat(ones(1,n), xTr);             % xTr = 7x93
-X = xTr';                                  % X = 93x7
-Y = yTr';                                  % Y = 93x1
-m_rmse = zeros(size(yTr'));    
-%% Naive LOOCV
-for k=1:size(xTr,2)                        % run 93 times
-    X(k,:) = [];                           % delete a kth sample
-    Y(k,:) = [];      
-    cur_n = size(X,1);                     % update n
-    w_hat = pinv(X)*Y;     
-    Y_hat = X*w_hat;
-    loocv_error = sum((Y - Y_hat).^2);
-    m_rmse(k) = sqrt((1/cur_n)*loocv_error);
-    X = xTr';                               % add kth back
-    Y = yTr';
+% xTr 6 features x 93 samples
+% yTr 1x93 labels
+% X 93 samples x 6 features 
+% y 93x1 labels
+X = xTr';                                  
+y = yTr';   
+% Number of Samples n = 93
+n = size(y,1);
+% Add bias feature (ones) to each sample
+% X 93 samples x 7 features 
+X = [ones(n,1) X];  
+%% Calculate rmse slowly
+% Matrix that hold rmse values at each iteration
+m_loocv_error = zeros(n,1);   
+% Make temporary X and y matrices
+Xtemp = X;
+ytemp = y;
+for i=1:n
+    % Step 1. Train on all data points, except xi
+    % Remove the ith sample from temporary X and y
+    Xtemp(i,:) = [];
+    ytemp(i,:) = [];
+    % Calculate w
+    w = pinv(Xtemp'*Xtemp)*Xtemp'*ytemp;
+    % Use that to get best estimate at all samples (including xi)
+    y_hat = X*w;
+    % Step 2. Predict yi for that removed sample
+    m_loocv_error(i) = (y(i) - y_hat(i))^2;
+    % Reset temporary X and y
+    Xtemp = X;
+    ytemp = y;
 end
-rmse = min(m_rmse);
-end
+loocv = sum(m_loocv_error);
+rmse = sqrt((1/n)*loocv);
