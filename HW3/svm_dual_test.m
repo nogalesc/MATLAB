2d516e1cfa1dcef82de9359e26c8cf6cd05bdca9
nogalesc <nogalesc@vt.edu>
2015-04-15 09:46:42 -0400

    Some changes to dual_SVM

--- HW3/svm_dual_test.m
+++ HW3/svm_dual_test.m
@@ -1,6 +1,6 @@
 % Test SVM dual results
 load('hw3_raw_data.mat','testdata');    % First extract test data = 10k x 28
-load('dual_SVM_results_w_w0.mat');      % Load w and w0
+load('dual_SVM_w_w0_try3.mat');      % Load w and w0
 y = testdata*w + w0;
 n = size(testdata,1);  
 % Make special matrix of alpha-numeric entries 
@@ -10,4 +10,4 @@ A = num2cell(predictions);
 myCell = {'EventID','Prediction'};
 finalAnswer =vertcat (myCell,A);
 % Uncomment the following line to save data to excel sheet:
-% xlswrite('Nogales_Chris_HW3.xls',finalAnswer)
+xlswrite('Nogales_Chris_HW3_try3.xls',finalAnswer)
