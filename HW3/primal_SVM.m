687aa304de3446a6698071ed19b01d6540d99adb
nogalesc@vt.edu <nogalesc@vt.edu>
2015-04-15 13:15:20 -0400

    Updated primal SVM

--- HW3/primal_SVM.m
+++ HW3/primal_SVM.m
@@ -6,23 +6,22 @@ C = 10;
 y = traindata(:,29);
 X = traindata;
 X(:,29) = [];
-% We have 28 features, 10,000 samples, last col is label
-ntrain = size(X,1);            % matrix 10000
-ndim = size(X,2);              % matrix 28
-% size of z = [w w0 ei]
-sz_z = ndim + ntrain + 1;      % 10,029
+ntrain = size(X,1);            % 10,000
+ndim = size(X,2);              % 28
+sz_z = ndim + 1 + ntrain;      % % size of z = [w w0 ei] => 10,029
 % construct H
 H = zeros(sz_z);               % 10,029 x 10,029
 H(1:ndim,1:ndim) = eye(ndim);
 % construct f
-f = [zeros(ndim,1); zeros(1); C*ones(ntrain,1)];
+f = [zeros(ndim,1); zeros(1); C*ones(ntrain,1)];  %10,029x1
 % construct A
 A_1 = zeros(size(X));
 for i = 1:size(y,1)
-    A_1(i) = y(i).*(X(i));
+    A_1(i) = (-1)*y(i).*(X(i));
 end
-A_2 = eye(ntrain);
-A_3 = y;
+A_2 = -y;
+A_3 = eye(ntrain);
+
 A = horzcat(A_1,A_2,A_3);
 % construct b
 b = (-1)*ones(size(y));
@@ -40,7 +39,7 @@ save('priaml_SVM_results_w_w0.mat','z','err','lm','w','w0');
 
 % tic;xqpas=qpas(H,f,L,k,A,b,l,u,dsp);toc
 % toc
-%                                                                                                                                                                                                                                                                                                                                                                       
+%                                                                                                                                                                                                                                                                                                                                                                      
 %  In Matlab a call to this function would look like:
 %
 %    [x,err,lm] = qp(H,f,L,k,A,b,l,u,display);
@@ -49,8 +48,8 @@ save('priaml_SVM_results_w_w0.mat','z','err','lm','w','w0');
 %
 %        H: An (n x n) positive semi-definite symmetric matrix
 %
-%	     f: A n element column vector
-%	
+%         f: A n element column vector
+%   
 %    (L,k): General linear inequality constraints
 %
 %    (A,b): General linear equality constraints
@@ -67,6 +66,4 @@ save('priaml_SVM_results_w_w0.mat','z','err','lm','w','w0');
 %
 %            err: error number, if err=0, then x is optimal
 %
-%             lm: structure of Lagrange multipliers 
-
-
+%             lm: structure of Lagrange multipliers
