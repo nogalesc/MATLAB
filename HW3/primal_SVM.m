b5831af8162c8dc209cc83648cf8633a02ef3ec1
Chris Nogales <nogalesc@vt.edu>
2015-04-15 18:06:04 -0400

    correct primal_SVM implementation

--- HW3/primal_SVM.m
+++ HW3/primal_SVM.m
@@ -15,27 +15,24 @@ sz_z = ndim + ntrain + 1;      % 10,029
 H = zeros(sz_z);               % 10,029 x 10,029
 H(1:ndim,1:ndim) = eye(ndim);
 % construct f
-f = [zeros(ndim,1); zeros(1); C*ones(ntrain,1)];
+f = [zeros(ndim,1); zeros(1); C*ones(ntrain,1)];  % 10,029
 % construct A
-A_1 = zeros(size(X));
-for i = 1:size(y,1)
-    A_1(i) = y(i).*(X(i));
-end
-A_2 = eye(ntrain);
-A_3 = y;
-A = horzcat(A_1,A_2,A_3);
+A = -[(y*ones(1,ndim)).*X  y eye(ntrain)];
 % construct b
 b = (-1)*ones(size(y));
 % construct lb = Represents the lower bounds elementwise in lb ≤ x ≤ ub.
-lb = zeros(size(f));
+lb = [-inf(1,ndim) -inf zeros(1,ntrain)];
 
 tic
-[z err lm] = qpas(H,f,A,b,[],[],lb);       % THIS ONE WORKED.
+[z err lm] = quadprog(H,f,A,b,[],[],lb);       % THIS ONE WORKED.
+
+% [z err lm] = qpas(H,f,A,b,[],[],lb);       % THIS ONE WORKED.
 toc
 
 
-w = X'*(z.*y);
-w0 = sum(y - (X*svmModel.w))/ntrain;            % My bias is w0
+w = z(1:ndim,1);
+w0 = z(ndim+1,1);
+% w0 = sum(y - (X*svmModel.w))/ntrain;            % My bias is w0
 save('priaml_SVM_results_w_w0.mat','z','err','lm','w','w0');
 
 % tic;xqpas=qpas(H,f,L,k,A,b,l,u,dsp);toc
