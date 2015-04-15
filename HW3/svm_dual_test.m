bce68bab3bab2ee70b848fdec0011fd61d02eff8
Chris Nogales <nogalesc@vt.edu>
2015-04-14 21:55:22 -0400

    Test file

---
+++ HW3/svm_dual_test.m
@@ -0,0 +1,13 @@
+% Test SVM dual results
+load('hw3_raw_data.mat','testdata');    % First extract test data = 10k x 28
+load('dual_SVM_results_w_w0.mat');      % Load w and w0
+y = testdata*w + w0;
+n = size(testdata,1);  
+% Make special matrix of alpha-numeric entries 
+my_ID = [1:n];
+predictions = horzcat(my_ID',y);
+A = num2cell(predictions);
+myCell = {'EventID','Prediction'};
+finalAnswer =vertcat (myCell,A);
+% Uncomment the following line to save data to excel sheet:
+% xlswrite('Nogales_Chris_HW3.xls',finalAnswer)
